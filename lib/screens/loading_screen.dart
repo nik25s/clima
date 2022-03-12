a851200f1f7ad2e89d91b26a7bb253abcd63e453
nik25s <90770057+nik25s@users.noreply.github.com>
2022-03-12 15:43:18 +0530

    add spinkit package

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,9 +1,11 @@
+import 'package:clima/screens/location_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:clima/service/location.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:clima/service/networking.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 const apikey = '84c77b0a6c6eca0f2cec59fd38201f08';
 
@@ -26,15 +28,25 @@ class _LoadingScreenState extends State<LoadingScreen> {
     await lc.getCurrentLocation();
     latitude = lc.latitudes;
     longitude = lc.longitudes;
-
+    var gett;
     NetworkHelper networkHelper = NetworkHelper(
         "https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$apikey");
 
     var weatherData = await networkHelper.getData();
+    Navigator.push(context, MaterialPageRoute(builder: (context) {
+      return LocationScreen();
+    }));
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: SpinKitDoubleBounce(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
   }
 }
